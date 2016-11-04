f5f13336779c92001d338050076b399442afdff7
Jared Pereira <jared@otlw.co>
2016-11-04 18:24:05 -0400

    Move addMember from registry to group.sol, add stake

--- group.sol
+++ group.sol
@@ -1,8 +1,9 @@
-import ../user.sol
+import ../assess/user.sol
 
 contract Group
 {
   uint public size;
+  uint public stake;
   bytes32 public goal;
   address[] public members;
   address[] public requirements;
@@ -19,21 +20,46 @@ contract Group
     _;
   }
 
-  function Group(address _concept, uint _size, address[] _requirements)
+  modifier onlyThis()
+  {
+    if(msg.sender != address(this))
+    {
+      throw;
+    }
+    _;
+  }
+
+  function Group(address _concept, uint _size, uint _stake, address[] _requirements)
   {
     concept = _concept;
     registry = msg.sender;
     requirements = _requirements;
-    members.push(member);
+    addMember(msg.sender);
     size = _size;
+    stake = _stake;
   }
 
-  function addMember(address member) onlyRegistry()
+  function addMember(address member) returns(bool) 
   {
-    if(members.length() < size)
+    if(members.length() >= size)
     {
-      members.push(member);
+      return false;
     }
+
+    for (uint i = 0, i <= requirements.length(), i++)
+    {
+      if(User(member).getConceptPassed(requirements[i]) == false)
+      {
+        return false;
+      }
+    }
+
+    if (User(member)).extTransferTokens(address(this), stake) != true)
+    {
+      return false;
+    }
+    members.push(member);
+    return true;
   }
 
   function setDone()
@@ -49,4 +75,12 @@ contract Group
     //Call some thing to do if the whole group is done here
   }
 
+  function makeAssessment(address user, address concept, uint time, uint size) onlyThis returns bool
+  {
+    return User(user).extStartAssessment(concept, time, size);
+  }
+
+  function getStake(address user, uint value) {
+    return User(user).extTransferTokens(address(this), value);
+  }
 }
